4a8e7485d9d36cc35290b9e1eab131d1cce6d58c
Methminpulsara <methminpulsara10@gmail.com>
2025-06-22 08:40:14 +0530

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { LogerService } from '../../service/LogerService';
+import { CommonModule, NgFor } from '@angular/common';
+import { CartService } from '../../service/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  count = 0;
 
+
+  constructor(public cartService :CartService) {}
+
+  onIncrementCount() {
+
+    let count = this.cartService.count() +  1;
+    this.cartService.count.set(count);
+
+  }
 }
