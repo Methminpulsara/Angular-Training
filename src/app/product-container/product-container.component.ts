a7a1acf52e0f934eaaa8172c1de7f1ef4364777c
Methminpulsara <methminpulsara10@gmail.com>
2025-06-29 12:28:17 +0530

    update

--- src/app/product-container/product-container.component.ts
+++ src/app/product-container/product-container.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductComponent } from "../product/product.component";
 import { Product } from '../modals/product';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { CartService } from '../../service/cart.service';
 import { ProductService } from '../../service/product.service';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-container',
-  imports: [ProductComponent, NgFor],
+  standalone: true,
+  imports: [ProductComponent, NgFor, CommonModule, FormsModule],
   templateUrl: './product-container.component.html',
-  styleUrl: './product-container.component.scss',
+  styleUrls: ['./product-container.component.scss'],
 })
 export class ProductContainerComponent implements OnInit {
+  productList: Product[] = [];
+
+  public categories: string[] = [];
+  selectedCategory: string = 'All';
+  filteredProductList: Product[] = [];
+
   constructor(
     private cartService: CartService,
     private productService: ProductService
   ) {}
-  ngOnInit(): void {
-   this.getAllProducts();
-  }
 
-  productList: Product[] = [];
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
 
   productOutPutEvent(product: Product) {
     this.cartService.addToCart(product);
@@ -29,15 +35,29 @@ export class ProductContainerComponent implements OnInit {
 
   getAllProducts() {
     this.productService.getAllItems().subscribe((data) => {
+      this.categories = Array.from(new Set(data.map(product => product.category)));
       this.productList = data;
-    })
+      this.filteredProductList = data;
+    });
+  }
+
+  filterByCategory(name: string) {
+    this.selectedCategory = name;
+
+    if (name.toLowerCase() === 'all') {
+
+      this.filteredProductList = this.productList;
+    } else {
+      this.filteredProductList = this.productList.filter(product =>
+        product.category.toLowerCase() === name.toLowerCase()
+      );
+    }
   }
 
 
-
-
-
-
-
+  
+categoryChange(name: string) {
+  this.filterByCategory(name);
+}
 
 }
